c8d133df6c4b12f10100e488c119a94a7411d574
Daniil Sergeyevich Martyn <daniil.martyn@wsu.edu>
2016-02-29 18:25:26 -0800

    Puzzle square drawing working correctly. Installed tap recognizer, user can now select cells.

--- Sudoku/PuzzleView.swift
+++ Sudoku/PuzzleView.swift
@@ -12,8 +12,37 @@ class PuzzleView: UIView {
 
     var selectedSquare = (row : -1, col : -1)
     
-    
+    func tap(sender : UITapGestureRecognizer) {
+        let location = sender.locationInView(self)
+        let boardSquare = getBoardSquare()
+        
+        if (CGRectContainsPoint(boardSquare, location)) {
+            let squareSize = boardSquare.size.width/9
+            let col = Int((location.x - boardSquare.origin.x)/squareSize)
+            let row = Int((location.y - boardSquare.origin.y)/squareSize)
+            if (0 ..< 9) ~= col && (0 ..< 9) ~= row {
+                if row != selectedSquare.row || col != selectedSquare.col {
+                    selectedSquare.col = col
+                    selectedSquare.row = row
+                    self.setNeedsDisplay()
+                }
+            }
+        }
+        NSLog("tap \(selectedSquare)")
 
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("tap:"))
+        self.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("tap:"))
+        self.addGestureRecognizer(tapGestureRecognizer)
+    }
     
     func getBoardSquare() -> CGRect {
         let size = min(self.bounds.width, self.bounds.height) - margin/2
@@ -21,7 +50,7 @@ class PuzzleView: UIView {
         return CGRectMake(center.x - size/2, center.y - size/2, size, size)
     }
     
-    let margin : CGFloat = 10
+    let margin : CGFloat = 6
     
     override func drawRect(rect: CGRect) {
         
@@ -31,14 +60,33 @@ class PuzzleView: UIView {
         
         CGContextSetLineWidth(context, margin)
         UIColor.blackColor().setStroke()
-        CGContextStrokeRect(context, boardSquare)
         
-        UIColor.yellowColor().setFill()
+        let bigSquareSize = boardSquare.size.width / 3
         let squareSize = boardSquare.size.width / 9
+        
+        if (selectedSquare.row >= 0 && selectedSquare.col >= 0) {
+            UIColor.yellowColor().setFill()
+            let x = boardSquare.origin.x + CGFloat(selectedSquare.col)*squareSize
+            let y = boardSquare.origin.y + CGFloat(selectedSquare.row)*squareSize
+            CGContextFillRect(context, CGRectMake(x, y, squareSize, squareSize))
+        }
+
+        for r in 0 ..< 3 {
+            for c in 0 ..< 3 {
+                CGContextStrokeRect(context, CGRectMake(boardSquare.origin.x + CGFloat(c) * bigSquareSize,
+                    boardSquare.origin.y + CGFloat(r) * bigSquareSize,
+                    bigSquareSize,
+                    bigSquareSize))
+            }
+        }
+        
+        CGContextSetLineWidth(context, margin/2)
         for r in 0 ..< 9 {
             for c in 0 ..< 9{
-                CGContextFillRect(context, CGRectMake(boardSquare.origin.x + CGFloat(c)*squareSize, boardSquare.origin.y + CGFloat(r)*squareSize,
-                    squareSize, squareSize))
+                CGContextStrokeRect(context, CGRectMake(boardSquare.origin.x + CGFloat(c)*squareSize,
+                    boardSquare.origin.y + CGFloat(r)*squareSize,
+                    squareSize,
+                    squareSize))
             }
         }
         
